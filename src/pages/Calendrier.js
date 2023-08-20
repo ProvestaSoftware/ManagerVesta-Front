7aa46ef10b6491b32c9b4cd6b527c204d45214c2
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2023-08-20 17:00:24 +0100

    Update Calendrier.js

--- src/pages/Calendrier.js
+++ src/pages/Calendrier.js
@@ -1,16 +1,26 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react'
 import ContentWrapper from '../components/ContentWrapper'
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { eventsData } from '../data/MockData'
 import PageTitle from '../components/PageTitle';
 import RegularDivider from '../components/RegularDivider';
 import '../assets/css/Calendrier.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { getChecks } from '../actions/checks';
 
 const localizer = momentLocalizer(moment)
 
 const Calendrier = () => {
+
+    const checks = useSelector((state) => state.checks);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getChecks());
+    }, []);
+
     return (
         <ContentWrapper>
             <div className='calendar-wrapper'>
@@ -19,10 +29,11 @@ const Calendrier = () => {
                 </div>
                 <RegularDivider />
                 <Calendar
+                    titleAccessor="num"
                     localizer={localizer}
-                    events={eventsData}
-                    startAccessor="start"
-                    endAccessor="end"
+                    events={checks}
+                    startAccessor="dueDate"
+                    endAccessor="dueDate"
                     style={{ height: 500 }}
                 />
             </div>
