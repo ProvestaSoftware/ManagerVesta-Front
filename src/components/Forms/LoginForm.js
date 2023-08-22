26b18f957b606224c872697b4d39c4fec5c66f68
Fourat Abdellatif <63204800+fouratabdellatif@users.noreply.github.com>
2023-08-22 09:43:41 +0100

    added error message Alert on login

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Input from '../Inputs/Input'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import RegularButton from '../Buttons/RegularButton';
 import CircularProgress from '../CircularProgress';
 import { login } from '../../actions/auth';
+import Alert from '../Alert';
 
 const LoginForm = () => {
 
     const dispatch = useDispatch();
     const [loader, setLoader] = useState(false);
+
+    const message = useSelector((state) => state?.auth?.error);
+    // console.log(message);
 
     const initState = {
         email: "",
@@ -38,11 +42,24 @@ const LoginForm = () => {
         await setLoader(false);
     };
 
+    const renderError = () => {
+        if (message) {
+            return (
+                <Alert style={{
+                    width: 'auto',
+                    margin: '30px auto 0 auto',
+                }}>
+                    {message}
+                </Alert>
+            )
+        }
+    }
+
     return (
         <div class="relative w-full max-h-full">
             <form onSubmit={handleSubmit} class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                 <div class="p-6 space-y-6">
-                    <div class="grid grid-cols-3 gap-6">
+                    <div class="grid grid-cols-3 gap-2">
                         <div class="col-span-3 sm:col-span-3">
                             <Input
                                 label="Email"
@@ -68,7 +85,12 @@ const LoginForm = () => {
                             />
                         </div>
                     </div>
+
+                    {renderError()}
+
                 </div>
+
+
                 <div class="p-6 border-t border-gray-200 rounded-b dark:border-gray-600">
                     {loader ? (
                         <CircularProgress />
